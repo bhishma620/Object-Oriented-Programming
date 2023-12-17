ab0513851f225a4a91e2bb48083c143d7a8324a5
Bhishmadev <bhishma620@gmail.com>
2023-12-17 16:03:34 +0530

    Update Encapsulation

---
+++ Encapsulation/src/Encapsulation.java
@@ -0,0 +1,37 @@
+class Student{
+    private String name;
+    private  int age;
+
+    void setName(String name){
+        this.name=name;
+    }
+    void setAge(int age){
+        this.age=age;
+    }
+    String getName(){
+        return name;
+    }
+    int getAge() {
+        return age;
+    }
+
+   public String toString(){
+       return "NAME: "+ this.name +" and AGE: "+ this.age;
+    }
+
+}
+public class Encapsulation {
+    public static void main(String[] args) {
+        Student ob=new Student();
+        ob.setName("Bhishma");
+        ob.setAge(20);
+
+        //printing details  by calling methods of Student class
+
+        System.out.println("STDUENT Name : " + ob.getName() +" , Age : " + ob.getAge());
+
+        //using toString()
+        System.out.println(ob.toString());
+
+    }
+}
